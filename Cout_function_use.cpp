6622b45cf32421e7097b34c73932fd8eb3fe8865
twinkal <twinkalmalaviya91@gmail.com>
2018-05-23 15:36:30 +0530

    Create Cout_function_use.cpp

---
+++ Cout_function_use.cpp
@@ -0,0 +1,16 @@
+/* starting program */
+/* Header file for intput output functions */
+
+#include<iostream> 
+
+using namespace std;
+
+/*main function where program will begin execution */
+int main()
+{
+	/* prints any string */
+	cout<<"Hello Twinkal"<<endl;
+	cout<<"Best luck with C++";
+	cout<<endl<<"i hope you will do go"<<endl;
+	return 0;
+}
